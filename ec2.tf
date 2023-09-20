d288f6ad3776361c41b79c844e0b3be296cd2c75
ola yusuf <olayusuf822@gmail.com>
2023-09-20 11:34:35 -0500

    update

--- ec2.tf
+++ ec2.tf
@@ -91,19 +91,19 @@ data "aws_ami" "amazon_linux_2" {
 }
 
 
+# launch the ec2 instance and install website
 resource "aws_instance" "ec2_instance" {
   ami                    = data.aws_ami.amazon_linux_2.id
   instance_type          = "t2.micro"
   subnet_id              = aws_default_subnet.default_az1.id
   vpc_security_group_ids = [aws_security_group.ec2_security_group.id]
-  key_name               = "myec2-keypair.pem"
+  key_name               = "my-ec2key.pem"
   user_data              = file("install_techmax.sh")
 
   tags = {
     Name = "techmax server"
   }
 }
-
 
 
 # print the url of the server
